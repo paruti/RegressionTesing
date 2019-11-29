d6e3439129cdfc3d765b9f66152caf9f4480dc98
paruti <parthiraj93@gmail.com>
2019-11-29 15:16:18 +0530

    ms

--- src/test/java/org/pgm/MissingNumber.java
+++ src/test/java/org/pgm/MissingNumber.java
@@ -5,11 +5,6 @@ import java.util.Iterator;
 import org.etsi.uri.x01903.v13.impl.GenericTimeStampTypeImpl;
 
 public class MissingNumber {
-public static void main(String[] args) {
-	int a[] = {1,2,3,5,6,7};
 
-	for (int i : a) {
-		register[i]=1;
-	}
-}
+
 }
